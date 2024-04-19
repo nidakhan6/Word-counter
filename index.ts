3f3d0842429011675b4a5bb1679eac64fe7a73af
NidaWorld <160415456+nidakhan6@users.noreply.github.com>
2024-04-19 15:12:45 +0500

    This is word counter

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+//SHABANG
+// import the 'inquirer' module which is a command line interface for node.js
+import inquirer from "inquirer"
+//1st question
+
+let question: {
+ sentence:string
+
+} = await inquirer.prompt([{
+
+ name: "sentence",
+ message:"enter your sentence to count the word",
+ type:"input"
+}])
+
+// create a variable for removing white space we use trim and split for arranging words by using inverted commas " "
+
+const words =question.sentence.trim().split(" ")
+
+//printing an array of words to the console
+
+console.log(words)
+
+//print the word count of the sentence to the console
+
+console.log(`your sentence word count is ${words.length}`)
+
